6f0f69edd259159835f06d96cd930a99f39c3740
Daniel Tennant <danieltennant@daniel-tennants-macbook-pro.local>
2015-04-13 23:30:06 +0100

    Initial Commit

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'yaml'
+require 'haml'
+
+#Import YAML file that describes hash indexes
+def YamlImport(yaml_file)
+  parsed = begin
+    YAML.load(File.open(yaml_file))
+  rescue ArgumentError => e
+    puts "Could not parse YAML: #{e.message}"
+  end
+end
+
+#Lookup invitee name based on code
+def code_lookup(code)
+  invitees = YamlImport('invitees.yml')
+  return invitees.fetch(code.to_i)['name']
+end
+
+get '/wedding/:name' do
+  invitee = code_lookup(params[:name])
+  haml :index, :locals => {:invitee => invitee}
+end
